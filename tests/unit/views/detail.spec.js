36d73915272b5484995e73aa6212806a1f51576a
Yuwen <648813710@qq.com>
2019-05-27 23:32:11 +0800

    提交修改

--- tests/unit/views/detail.spec.js
+++ tests/unit/views/detail.spec.js
@@ -8,7 +8,8 @@ import detail from '@/views/detail.vue' // 引入页面 detail
 //模拟 vuex
 const localVue = createLocalVue() //模拟一个 vue 实例
 localVue.use(vuex) //使用vuex
-let mockFn=jest.fn(e);
+let mockFn=jest.fn();
+let mockFn1=jest.fn();
 const store = new vuex.Store({
     state: {
         herodata: [{
@@ -19,7 +20,7 @@ const store = new vuex.Store({
     },
     actions:{
         saveUserAct:mockFn,
-        addmsgAct:mockFn,
+        addmsgAct:mockFn1,
     }
 }); //模拟store数据
 
@@ -39,9 +40,6 @@ let wrapper = shallowMount(detail, {
             params: {
                 id: 1
             }
-        },
-        $router: {
-            go: mockFn
         }
     }
 }); //挂载节点前 放入实例和 vuex 以及data,使用 shallowMount 浅渲染，不渲染子组件
@@ -55,16 +53,33 @@ describe("测试 details 页面", () => {
         let html_ = `<div class=\"details\"><h1>王哈哈的主页</h1> <p>id：1</p> <div class=\"name\"><p>姓名</p> <div><input type=\"text\"></div> <div class=\"button\"><span>返回</span> <span>保存</span></div></div> <msg-stub msg=\"获取到英雄们\"></msg-stub></div>`;
         expect(wrapper.html()).toContain(html_); //返回预定值 说明模拟的数据正常 且created里的方法运行正常
     });
+
     //测试 saveUser 函数
     test("测试 saveUser 方法", () => {
+        const mockFns = jest.fn();
+        const mockFns1 = jest.fn();
+        const mockFns2 = jest.fn();
+        mockFns();
+        mockFns1();
+        mockFns2();
+        wrapper.setMethods({goBack:mockFns,storeData:mockFns1,userDetail:mockFns2});//模拟方法
         wrapper.find("div.name input").setValue("123"); //设置input 值为模拟值
         expect(wrapper.vm.name).toBe('123'); //查看属性是否跟着变化
         wrapper.findAll('div.button span').at(1).trigger("click"); //变化后点击保存
+        expect(mockFns).toHaveBeenCalled();//确保方法执行
+        expect(mockFns1).toHaveBeenCalled();//确保方法执行
+        expect(mockFns2).toHaveBeenCalled();//确保方法执行
+
+        expect(mockFn).toHaveBeenCalled(); //查看方法是否回调
+        expect(mockFn1).toHaveBeenCalled(); //查看方法是否回调
     });
 
     //测试 goBack 函数
     test("测试 goBack 方法", () => {
+        const mockFns = jest.fn();
+        mockFns();
+        wrapper.setMethods({goBack:mockFns});//模拟方法
         wrapper.find('div.button span').trigger("click"); //变化后点击保存
-        expect(wrapper.vm.goBack).toBeTruthy();//确保方法执行
+        expect(mockFns).toHaveBeenCalled();//确保方法执行
     });
 });
